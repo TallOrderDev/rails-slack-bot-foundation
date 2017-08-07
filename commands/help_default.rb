ebca443a9d50402591a02a8e3772a2a6d680e13e
John Gill <johngill@tallorderdev.com>
2017-08-06 22:29:42 -0700

    Remove Monkey Patch for help

--- commands/help_default.rb
+++ commands/help_default.rb
@@ -1,34 +1,34 @@
-module SlackRubyBot
-  module Commands
-    class HelpCommand < Base
-      help do
-        title 'help'
-        desc 'Shows help information.'
-      end
-
-      command 'help' do |client, data, match|
-        command = match[:expression]
-
-        text = if command.present?
-                 CommandsHelper.instance.command_full_desc(command)
-               else
-                 general_text
-               end
-
-        client.say(channel: data.channel, text: text, gif: 'help')
-      end
-
-      class << self
-        private
-
-        def general_text
-          bot_desc = CommandsHelper.instance.bot_desc_and_commands
-          other_commands_descs = CommandsHelper.instance.other_commands_descs
-          <<TEXT
-#{bot_desc.join("\n")}
-TEXT
-        end
-      end
-    end
-  end
-end
+# module SlackRubyBot
+#   module Commands
+#     class HelpCommand < Base
+#       help do
+#         title 'help'
+#         desc 'Shows help information.'
+#       end
+#
+#       command 'help' do |client, data, match|
+#         command = match[:expression]
+#
+#         text = if command.present?
+#                  CommandsHelper.instance.command_full_desc(command)
+#                else
+#                  general_text
+#                end
+#
+#         client.say(channel: data.channel, text: text, gif: 'help')
+#       end
+#
+#       class << self
+#         private
+#
+#         def general_text
+#           bot_desc = CommandsHelper.instance.bot_desc_and_commands
+#           other_commands_descs = CommandsHelper.instance.other_commands_descs
+#           <<TEXT
+# #{bot_desc.join("\n")}
+# TEXT
+#         end
+#       end
+#     end
+#   end
+# end
